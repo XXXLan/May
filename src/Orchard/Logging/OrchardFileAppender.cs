cf3655dc22966b02f1c613ace8d41fe600bc2353
Durian <lanchao@outlook.com>
2015-05-17 19:08:44 +0800

    add logging

---
+++ src/Orchard/Logging/OrchardFileAppender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using log4net.Appender;
+using log4net.Util;
+
+namespace Orchard.Logging
+{
+    public class OrchardFileAppender:RollingFileAppender
+    {
+        /// <summary>
+        /// Dictionary of already known suffixes(based on previous attempts) for a given filename.
+        /// </summary>
+        private static readonly Dictionary<string, int> _suffixes = new Dictionary<string, int>();
+
+        /// <summary>
+        /// The number of suffix attempts that will be made on each OpenFile method call.
+        /// </summary>
+        private const int Retries = 50;
+
+        private const int MaxSuffixex = 100;
+
+    }
+}
