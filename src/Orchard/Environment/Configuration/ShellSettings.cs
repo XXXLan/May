3dfe4c7273c03779c10039ca5ba2228037bce851
Durian <lanchao@outlook.com>
2015-05-18 23:55:58 +0800

    add ShellSettings

--- src/Orchard/Environment/Configuration/ShellSettings.cs
+++ src/Orchard/Environment/Configuration/ShellSettings.cs
@@ -36,12 +36,127 @@ namespace Orchard.Environment.Configuration
         /// </summary>
         public IEnumerable<string> Keys { get { return _values.Keys; } }
 
+		/// <summary>
+		/// The name of tenant
+		/// </summary>
         public string Name 
         {
             get { return this["Name"] ?? ""; }
             set { this["Name"] = value; }
         }
 
+		/// <summary>
+		/// The database provider for tenant
+		/// </summary>
+		public string DataProvider{
+			get{ return this ["DataProvider"] ?? "";}
+			set{this ["DataProvider"] = value;}
+		}
 
+		/// <summary>
+		/// The database connection string
+		/// </summary>
+		public string DataConnectionString{
+			get{ return this ["DataConnectionString"];}
+			set{ this ["DataConnectionString"] = value;}
+		}
+
+		/// <summary>
+		/// The data table prefix added to table names for this tenant
+		/// </summary>
+		public string DataTablePrefix{
+			get{ return this ["DataTablePrefix"];}
+			set{ this ["DataTablePrefix"] = value;}
+		}
+
+		/// <summary>
+		/// The host name of the tenant
+		/// </summary>
+		public string RequestUrlHost{
+			get{ return this ["RequestUrlHost"];}
+			set{ this ["RequestUrlHost"] = value;}
+		}
+
+		/// <summary>
+		/// The request url prefix of the tanant
+		/// </summary>
+		public string RequestUrlProfix{
+			get{ return this ["RequestUrlPrefix"];}
+			set{ _values ["RequestUrlProfix"] = value;}
+		}
+
+		/// <summary>
+		/// The encryption algorithm used for encryption services
+		/// </summary>
+		public string EncryptionAlgorithm{
+			get{ return this ["EncryptionAlgorithm"];}
+			set{ this ["EncryptionAlgorithm"] = value;}
+		}
+
+		/// <summary>
+		/// The encryption key used for encryption services
+		/// </summary>
+		/// <value>The encryption key.</value>
+		public string EncryptionKey{
+			get{ return this ["EncryptionKey"];}
+			set{ _values ["EncryptionKey"] = value;}
+		}
+
+		/// <summary>
+		/// The hash algorithm used for encryption services
+		/// </summary>
+		public string HashAlgorithm{
+			get{ return this ["HashAlgorithm"];}
+			set{ this ["HashAlgorithm"] = value;}
+		}
+
+		/// <summary>
+		/// The hash key used for encryption services
+		/// </summary>
+		public string HashKey{
+			get{ return this ["HashKey"];}
+			set{ this ["HashKey"] = value;}
+		}
+
+		/// <summary>
+		/// List of available themes for this tenant
+		/// </summary>
+		public string[] Themes{
+			get{ 
+				return _themes??(Themes=(_values["Themes"]??"").Split(new[]{';'},StringSplitOptions.RemoveEmptyEntries))
+															   .Select(t=>t.Trim())
+															   .ToArray();
+			}
+			set{
+				_themes = value;
+				this ["Themes"] = string.Join (";", value);
+			}
+		}
+
+		/// <summary>
+		/// List of available modules for this tenant
+		/// </summary>
+		public string[] Modules{
+			get{
+				return _modules ?? (Modules = (_values ["Modules"] ?? "").Split (new[]{ ';' }, StringSplitOptions.RemoveEmptyEntries))
+																		 .Select (t => t.Trim ())
+																		 .ToArray ();
+			}
+			set{
+				_modules = value;
+				this ["Modules"] = string.Join (";", value);
+			}
+		}
+
+		/// <summary>
+		/// The state is which the tenant is
+		/// </summary>
+		public TenantState State{
+			get{return _tenantState;}
+			set{
+				_tenantState = value;
+				this ["State"] = value.ToString ();
+			}
+		}
     }
 }
--- src/Orchard/Logging/OrchardLog4netLogger.cs
+++ src/Orchard/Logging/OrchardLog4netLogger.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using Orchard.Environment;
+using Orchard.Environment.Configuration;
+using log4net;
+using log4net.Core;
+using log4net.Util;
 
 using Logger = Castle.Core.Logging.ILogger;
 
@@ -11,6 +15,12 @@ namespace Orchard.Logging
     {
         private static readonly Type declaringType = typeof(OrchardLog4netLogger);
 
-        private readonly Lazy<Shell>
+		private readonly Lazy<ShellSettings> _shellSettings;
+
+		public IOrchardHostContainer HostContainer{ get; set;}
+
+		public OrchardLog4netLogger(log4net.Core.ILogger logger,OrchardLog4netFactory factory){
+			
+		}
     }
 }
