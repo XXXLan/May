38dd2f8d8847a00d15054f65ed4d939d416f2573
Durian <lanchao@outlook.com>
2015-05-19 23:25:12 +0800

    20150519 at home

--- src/Orchard/Logging/OrchardLog4netFactory.cs
+++ src/Orchard/Logging/OrchardLog4netFactory.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using Castle.Core.logging;
+using Castle.Core.Logging;
 using log4net;
 using log4net.Config;
+using Orchard.Environment;
 
 namespace Orchard.Logging
 {
-    public class OrchardLog4netFactory:
+	public class OrchardLog4netFactory:AbstractLoggerFactory
     {
+		private static bool _isFileWatched=false;
+
+		public OrchardLog4netFactory( hostEnviroment)
+		{
+		}
+
+		public OrchardLog4netFactory(string configFilename)
     }
 }
